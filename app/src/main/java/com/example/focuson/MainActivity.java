f43aec37ee148be83d300260ef67e21a4a9bf1fb
Marc <marcdominguezr@gmail.com>
2019-03-09 02:33:29 +0100

    Oh my my, oh hell yes

--- app/src/main/java/com/example/focuson/MainActivity.java
+++ app/src/main/java/com/example/focuson/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.focuson;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        navigationView.getMenu().getItem(0).setChecked(true);
+
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                        menuItem.setCheckable(true);
+                        drawerLayout.closeDrawers();
+
+                        return true;
+                    }
+                }
+        );
+
     }
+
+
 }
